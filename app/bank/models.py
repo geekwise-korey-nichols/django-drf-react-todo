c64f5f6b65420c6a375b94132bad4fc900d24220
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2019-12-11 20:32:07 +0000

    added: models for branch and customer

--- app/bank/models.py
+++ app/bank/models.py
@@ -12,7 +12,18 @@ class Branch(models.Model):
         return f"{self.location_name} - {self.location}"
     
 # Customer model
+class Customer(models.Model):
+    branch = models.ForeignKey(
+        Branch,
+        on_delete = models.CASCADE
+    )
+    customer_name = models.CharField(max_length=50)
+    customer_email = models.EmailField(max_length=300)
+
+    def __str__(self):
+        return f"{self.branch} - {self.customer_name}"
 
 # Account model
-
+class Account(models.Model):
+    
 # Product model
