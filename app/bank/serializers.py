99e002367f53d0fdd20e9f23de5a38bc7cf06749
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2019-12-12 19:06:44 +0000

    added: urls for drf and updated serializers

--- app/bank/serializers.py
+++ app/bank/serializers.py
@@ -5,31 +5,31 @@ class Branch_Serializer( serializers.HyperlinkedModelSerializer ):
     class Meta:
         model = Branch
         fields = [
-            location_name,
-            location
+            'location_name',
+            'location'
         ]
 
 class Customer_Serializer( serializers.HyperlinkedModelSerializer ):
     class Meta:
         model = Customer
         fields = [
-            customer_name,
-            customer_email
+            'customer_name',
+            'customer_email'
         ]
 
 class Account_Serializers( serializers.HyperlinkedModelSerializer ):
     class Meta:
         model = Account
         fields = [
-            account_id
+            'account_id'
         ]
 
-class Product_Serializer( serializer.HyperlinkedModelSerializer ):
+class Product_Serializer( serializers.HyperlinkedModelSerializer ):
     class Meta:
         model = Product
         fields = [
-            amount,
-            product_type
+            'amount',
+            'product_type'
         ]
 
         
--- app/main/urls.py
+++ app/main/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from bank import views as bank_views
+
+router = routers.DefaultRouter()
+router.register(r'branch', bank_views.Branch_Viewset)
+router.register(r'customer', bank_views.Customer_Viewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
