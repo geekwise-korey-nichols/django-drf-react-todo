dde5722dea9e00d88353319c5c158192ede8b4a2
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2019-12-12 17:58:00 +0000

    added: views for branch and customer drf

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import Branch_Serializer, Customer_Serializer, Account_Serializers, Product_Serializer
+from .models import Branch, Customer, Account, Product
 
-# Create your views here.
+class Branch_Viewset(viewsets.ModelViewSet):
+    queryset = Branch.objects.all()
+    serializer_class = Branch_Serializer
+
+class Customer_Viewset(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = Branch_Serializer
