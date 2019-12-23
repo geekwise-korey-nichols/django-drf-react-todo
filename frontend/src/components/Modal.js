1bf9aecc35017fc751785ad3a63675b9aeae9f5e
geekwise-korey-nichols <geekwise.korey.nichols@gmail.com>
2019-12-23 13:16:01 -0800

    added frontend files to master

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+export default class CustomModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: this.props.activeItem
+    };
+  }
+  handleChange = e => {
+    let { name, value } = e.target;
+    const activeItem = { ...this.state.activeItem, [name]: value };
+    this.setState({ activeItem });
+  };
+  render() {
+    const { toggle, onSave } = this.props;
+    return (
+      <Modal isOpen={true} toggle={toggle}>
+        <ModalHeader toggle={toggle}> Todo Item </ModalHeader>
+        <ModalBody>
+        <Form>
+                <FormGroup>
+                  <Label for="location_name">Location Name</Label>
+                  <Input
+                    type="text"
+                    name="location_name"
+                    value={this.state.activeItem.location_name}
+                    onChange={this.handleChange}
+                    placeholder="Enter branch name"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="location">Location</Label>
+                  <Input
+                    type="text"
+                    name="location"
+                    value={this.state.activeItem.location}
+                    onChange={this.handleChange}
+                    placeholder="Enter branch location"
+                  />
+                </FormGroup>
+              </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+            Save
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
